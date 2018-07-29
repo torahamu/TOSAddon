dfc79881182397e531f91716a9e1351cbb38c2a5
torahamu <torahamu39@gmail.com>
2018-07-29 22:39:09 +0900

    del jptos_bugfix addchatemoticonlist

--- jptos_bugfix/addchatemoticonlist/src/addchatemoticonlist.lua
+++
@@ -1,7 +0,0 @@
-function ADDCHATEMOTICONLIST_ON_INIT(addon, frame)
-	local emoticonFrame = ui.GetFrame('chat_emoticon');
-	emoticonFrame:Resize(emoticonFrame:GetWidth(),emoticonFrame:GetHeight()+50)
-	local emoticons = GET_CHILD(emoticonFrame, "emoticons", "ui::CSlotSet");
-	emoticons:SetColRow(10,6);
-	emoticons:CreateSlots();
-end
